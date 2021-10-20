6a687e88adf63e48563bde1126b39b219a1e87e5
Bruno Moreira <bsmoreira96@gmail.com>
2021-10-20 12:28:36 -0300

    JavaScript file inclusion, and HTML tweaking

---
+++ script.js
@@ -0,0 +1,45 @@
+const button = document.querySelector('button')
+const select = document.getElementById('currency-select')
+
+const dolar = 5.2
+const euro = 5.9
+
+const convertValue = () => {
+    const inputReais = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+    const currencyValueText = document.getElementById('currency-value-text')
+
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: 'BRL' }
+    ).format(inputReais);
+
+    if (select.value === 'US$ Dólar Americano') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(inputReais / dolar);
+    } else if (select.value === '€ Euro') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'EUR' }
+        ).format(inputReais / euro);
+    }
+}
+
+changeCurrency = () => {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImg = document.getElementById('currency-img')
+
+    if (select.value === 'US$ Dólar Americano') {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImg.src = './assets/Estados-Unidos.png'
+    }
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = './assets/Euro.png'
+    }
+
+    convertValue()
+}
+
+button.addEventListener('click', convertValue)
+select.addEventListener("change", changeCurrency)
